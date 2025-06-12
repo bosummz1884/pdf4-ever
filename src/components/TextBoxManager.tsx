4ba4bbed2bc3bf19df3ca0352d8a4c5684465637
bosummz1884 <Brandonjulian16@gmail.com>
2025-06-12 08:15:31 -0500

    rm un wrk

--- src/components/TextBoxManager.tsx
+++ src/components/TextBoxManager.tsx
@@ -1,11 +1,6 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
 import { Rnd } from "react-rnd";
 import { nanoid } from "nanoid";
-import * as pdfjsLib from 'pdfjs-dist/build/pdf.mjs';
-import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.min.js?url';
-
-// Set the static worker URL for Cloudflare Pages compatibility
-pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
